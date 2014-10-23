3aed8becf495ed7db3fce6c46d502cebd6a1ecce
Chris Contolini <christopher.contolini@cfpb.gov>
2014-10-23 13:11:53 -0400

    Add a default port

--- server.js
+++ server.js
@@ -1,45 +1,42 @@
 var express = require('express');
-var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
-
 var data = {};
 
-app.get('/api', function(req, res){
+app.set('port', process.env.PORT || 8080);
+app.get('/3/album/:id/images', function (req, res) {
 
-  var url = 'https://imgur.com/a/1Oiej/layout/grid';
+  var url = 'https://imgur.com/a/' + req.params.id + '/layout/grid';
 
-  request(url, function(error, response, html) {
+  request(url, function (error, response, html) {
 
-    if ( !error ) {
+    console.log();
 
-      var $ = cheerio.load( html );
-      data.images = [];
-
-      $('.posts').find('.post').each(function(i, elem) {
-
-        var $this = $(this),
-            link = $this.find('a').attr('href');
-
-        var image = {
-          id: $this.attr('id') || '',
-          link: link ? 'https:' + link : '',
-        };
-
-        if ( image.link ) {
-          data.images.push( image );
-        }
-
-      });
-
+    if (error || response.statusCode !== 200) {
+      return res.status(404).send('Error finding that imgur album.');
     }
 
-    res.json( {data: data} );
+    var $ = cheerio.load(html);
+    data.images = [];
+
+    $('.posts').find('.post').each(function () {
+      var $this = $(this);
+      var link = $this.find('a').attr('href');
+      var image = {
+        id: $this.attr('id') || '',
+        link: link ? 'https:' + link : '',
+      };
+      if (image.link) {
+        data.images.push(image);
+      }
+    });
+
+    res.json({data: data});
 
   });
 
 });
 
-app.listen('8123');
+app.listen(app.get('port'));
 exports = module.exports = app;
